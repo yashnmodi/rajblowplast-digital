b263f6e9cf3751c61c587a1bd5a1230a78d60297
Yash Modi <myn.yashnm@gmail.com>
2022-06-05 17:44:21 +0530

    Spring boot setup initial commit

---
+++ src/main/java/com/rajblowplast/digital/RajBlowPlastSystemsApplication.java
@@ -0,0 +1,13 @@
+package com.rajblowplast.digital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RajBlowPlastSystemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RajBlowPlastSystemsApplication.class, args);
+	}
+
+}
