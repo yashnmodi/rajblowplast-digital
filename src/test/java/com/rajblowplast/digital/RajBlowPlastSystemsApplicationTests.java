0c5fe155578062b37ca70ecd79d4bb6ef6892031
Yash Modi <myn.yashnm@gmail.com>
2022-06-05 17:49:25 +0530

    Spring boot setup initial commit

---
+++ src/test/java/com/rajblowplast/digital/RajBlowPlastSystemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajblowplast.digital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RajBlowPlastSystemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
